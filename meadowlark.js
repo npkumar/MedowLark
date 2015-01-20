682dba954a2e0554d2e3515120a00069dbba9d21
Nitin Pradeep Kumar <nkumar@appcelerator.com>
2015-01-20 17:37:28 +0800

    session with mongodb

--- meadowlark.js
+++ meadowlark.js
@@ -125,8 +125,16 @@ app.use(function(req, res, next) {
 	next();
 });
 
+
+var MongoSessionStore = require('session-mongoose')(require('connect'));
+var sessionStore = new MongoSessionStore({
+	url: credentials.mongo.development.connectionString
+});
+
 app.use(require('cookie-parser')(credentials.cookieSecret));
-app.use(require('express-session')());
+app.use(require('express-session')({
+	store: sessionStore
+}));
 
 switch (app.get('env')) {
 	case 'development':
@@ -340,22 +348,72 @@ app.post('/contest/vacation-photo/:year/:month', function(req, res) {
 	});
 });
 
+app.get('/set-currency/:currency', function(req, res) {
+	req.session.currency = req.params.currency;
+	return res.redirect(303, '/vacations');
+});
+
+function convertFromUSD(value, currency) {
+	switch (currency) {
+		case 'USD':
+			return value * 1;
+		case 'GBP':
+			return value * 0.6;
+		case 'BTC':
+			return value * 0.0023707918444761;
+		default:
+			return NaN;
+	}
+}
+
+// app.get('/vacations', function(req, res) {
+// 	Vacation.find({
+// 		available: true
+// 	}, function(err, vacations) {
+// 		var context = {
+// 			vacations: vacations.map(function(vacation) {
+// 				return {
+// 					sku: vacation.sku,
+// 					name: vacation.name,
+// 					description: vacation.description,
+// 					price: vacation.getDisplayPrice(),
+// 					inSeason: vacation.inSeason,
+// 				}
+// 			})
+// 		};
+// 		res.render('vacations', context);
+// 	});
+// });
 
 app.get('/vacations', function(req, res) {
 	Vacation.find({
 		available: true
 	}, function(err, vacations) {
+		var currency = req.session.currency || 'USD';
 		var context = {
+			currency: currency,
 			vacations: vacations.map(function(vacation) {
 				return {
 					sku: vacation.sku,
 					name: vacation.name,
 					description: vacation.description,
-					price: vacation.getDisplayPrice(),
 					inSeason: vacation.inSeason,
+					price: convertFromUSD(vacation.priceInCents / 100, currency),
+					qty: vacation.qty,
 				}
 			})
 		};
+		switch (currency) {
+			case 'USD':
+				context.currencyUSD = 'selected';
+				break;
+			case 'GBP':
+				context.currencyGBP = 'selected';
+				break;
+			case 'BTC':
+				context.currencyBTC = 'selected';
+				break;
+		}
 		res.render('vacations', context);
 	});
 });
